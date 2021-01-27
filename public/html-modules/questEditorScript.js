ae5d165fac2e11bf79b97a9b8908f6414f95be47
miloscane <76661053+miloscane@users.noreply.github.com>
2021-01-27 16:52:55 +0100

    a

--- public/html-modules/questEditorScript.js
+++ public/html-modules/questEditorScript.js
@@ -61,4 +61,151 @@ function createQuestion(){
 	document.getElementById("questions-wrap").appendChild(questionWrap)
 }
 
+function removeQuestion(elem){
+	var questionIndex	=	false;
+	var questions	=	elem.parentElement.parentElement.parentElement.getElementsByClassName("questionWrap");
+	for(var i=0;i<questions.length;i++){
+		if(elem.parentElement.parentElement==questions[i]){
+			questionIndex	=	i;
+			break;
+		}
+	}
+
+	if(questionIndex || questionIndex==0){
+		elem.parentElement.parentElement.parentElement.removeChild(elem.parentElement.parentElement);
+		document.getElementById("question-numbers-list").removeChild(document.getElementById("question-numbers-list").getElementsByClassName("button")[questionIndex]);
+	}else{
+		console.log("Couldnt find which question to delete");
+	}
+}
+
+function imageUploaded(elem){
+	if(elem.files && elem.files[0]){
+		var reader = new FileReader();
+		reader.onload = function(e){
+			elem.parentElement.getElementsByClassName("image")[0].src=e.target.result;
+
+		}
+
+		reader.readAsDataURL(elem.files[0]);
+	}
+}
+
 addQuestion();
+
+function addAnswer(elem){
+	var questionType	=	false;
+	var questionTypes	=	elem.parentElement.parentElement.getElementsByClassName("questionType");
+	for(var i=0;i<questionTypes.length;i++){
+		if(questionTypes[i].getElementsByClassName("addAnswerButton")[0]==elem){
+			questionType	=	eval(i+1);
+		}
+	}
+
+	if(questionType || questionType==0){
+		switch (questionType){
+			case 1:
+				//True-false text
+				var answerWrap	=	document.createElement("DIV");
+				answerWrap.setAttribute("class","answerWrap");
+
+					var answer	=	document.createElement("DIV");
+					answer.setAttribute("class","button answer");
+
+						var answerInput	=	document.createElement("INPUT");
+						answerInput.setAttribute("type","text");
+						answerInput.setAttribute("class","answerTextInput");
+						answer.appendChild(answerInput);
+
+						var answerDelete	=	document.createElement("DIV");
+						answerDelete.setAttribute("class","killAnswer");
+						answerDelete.setAttribute("onclick","this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement)");
+						answerDelete.innerHTML="X";
+						answer.appendChild(answerDelete);
+
+					answerWrap.appendChild(answer);
+
+					var answerAttributes	=	document.createElement("DIV");
+					answerAttributes.setAttribute("class","answerAttributes");
+
+						var answerAttribute	=	document.createElement("DIV");
+						answerAttribute.innerHTML="<div style='display:inline-block'>Answer is True: </div><div style='display:inline-block;width:20px;'><input type='checkbox'></div>";
+						answerAttributes.appendChild(answerAttribute);
+
+					answerWrap.appendChild(answerAttributes);
+
+				questionTypes[questionType-1].getElementsByClassName("answersWrap")[0].appendChild(answerWrap);
+				break;
+			case 2:
+				//true-false image
+				var answer	=	document.createElement("DIV");
+				answer.setAttribute("class","imageAnswer");
+
+				questionTypes[questionType-1].getElementsByClassName("imageWrap")[0].appendChild(answer);
+				$(function() {
+					$(".imageAnswer").draggable({
+						"containment":$(".imageAnswer").parent()
+					}).resizable({
+						"containment":$(".imageAnswer").parent()
+					});
+
+				});
+				break;
+			case 3:
+				//Reorder actions
+				var answerWrap	=	document.createElement("DIV");
+				answerWrap.setAttribute("class","answerWrap");
+
+					var answer	=	document.createElement("INPUT");
+					answer.setAttribute("type","text");
+					answer.setAttribute("class","answer");
+					answerWrap.appendChild(answer);
+
+					var answerDelete	=	document.createElement("DIV");
+					answerDelete.setAttribute("class","killAnswer");
+					answerDelete.setAttribute("onclick","this.parentElement.parentElement.removeChild(this.parentElement)");
+					answerDelete.innerHTML="X";
+					answerWrap.appendChild(answerDelete);
+
+				questionTypes[questionType-1].getElementsByClassName("answersWrap")[0].appendChild(answerWrap);
+				break;
+			case 4:
+				//Connect questions
+				var answerWrap	=	document.createElement("DIV");
+				answerWrap.setAttribute("class","answerWrap");
+
+					var answer	=	document.createElement("DIV");
+					answer.setAttribute("class","answer");
+
+						var input	=	document.createElement("INPUT");
+						input.setAttribute("class","answerInput");
+						answer.appendChild(input);
+
+					answerWrap.appendChild(answer);
+
+					var answer	=	document.createElement("DIV");
+					answer.setAttribute("class","answer");
+
+						var input	=	document.createElement("INPUT");
+						input.setAttribute("class","answerInput");
+						answer.appendChild(input);
+
+					answerWrap.appendChild(answer);
+
+					var answerDelete	=	document.createElement("DIV");
+					answerDelete.setAttribute("class","killAnswer");
+					answerDelete.setAttribute("onclick","this.parentElement.parentElement.removeChild(this.parentElement)");
+					answerDelete.innerHTML="X";
+					answerWrap.appendChild(answerDelete);
+
+				questionTypes[questionType-1].getElementsByClassName("answersWrap")[0].appendChild(answerWrap);
+				break;
+			case 5:
+				//Tap on image and add a story
+
+				break;
+		}
+	}else{
+		console.log("couldn't figure out question type");
+	}
+}
